2f91e5c9f73b93ea45e266be4640457c6e7986fe
Neia <neeia2718@gmail.com>
2022-07-20 03:05:07 -0700

    Convert input.tsx to proper NextPage

--- src/pages/data/input.tsx
+++ src/pages/data/input.tsx
@@ -1,25 +1,8 @@
 import React, { useState } from "react";
+import type { NextPage } from "next";
 import { Operator } from "../../types/operator";
 import { Box } from "@mui/material";
 import OperatorSelector from "../../components/data/OperatorSelector";
-
-interface Props {
-  operators: Record<string, Operator>;
-  changeOperators: (
-    operatorID: string,
-    property: string,
-    value: number | boolean,
-    index?: number
-  ) => void;
-  presets: Record<string, Operator>;
-  changePresets: (
-    presetID: string,
-    property: string,
-    value: number | boolean
-  ) => void;
-  applyBatch: (source: Operator, target: string[]) => void;
-}
-
 
 export enum SELECT_STATE {
   Grid,
@@ -28,22 +11,11 @@ export enum SELECT_STATE {
   Batch
 }
 
-export const COLOR_BY_RARITY = ["#000000", "#9f9f9f", "#dce537", "#00b2f6", "#dbb1db", "#fbae02", "#f96601"]
-
-
-const DataTab = React.memo((props: Props) => {
+const Input: NextPage = () => {
   const [selectedOperator, setSelectedOperator] = React.useState("")
   const [selectedPreset, setSelectedPreset] = useState("")
   const [selectState, setSelectState] = React.useState(SELECT_STATE.Grid);
   const [selectBatchOps, setSelectBatchOps] = useState<string[]>([])
-
-  const editSelectionGrid =
-    <OperatorSelector
-      onClick={(op: Operator) => {
-        setSelectedOperator(op.id);
-        setSelectState(SELECT_STATE.OpEdit)
-      }}
-    />
 
   return (
     <Box sx={{
@@ -75,5 +47,5 @@ const DataTab = React.memo((props: Props) => {
       </Box>
     </Box>
   );
-});
-export default DataTab;
+}
+export default Input;
