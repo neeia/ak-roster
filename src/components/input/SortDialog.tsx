c47b14af860f9cfc1bef033e7eb8ebed6967fef2
Neia <neeia2718@gmail.com>
2022-08-21 21:58:06 -0700

    intermediate commit

--- src/components/input/SortDialog.tsx
+++ src/components/input/SortDialog.tsx
@@ -6,24 +6,14 @@ import { Tune } from "@mui/icons-material";
 import { SortListItem } from "../../pages/data/input";
 
 
-const sortFunctions = {
-  "None": (): number => 0,
-  "Name": (a: Operator, b: Operator): number => b.name.localeCompare(a.name),
-  "Level": (a: Operator, b: Operator): number => a.level - b.level,
-  "Rarity": (a: Operator, b: Operator): number => a.rarity - b.rarity,
-  "Promotion": (a: Operator, b: Operator): number => a.promotion - b.promotion,
-  "Potential": (a: Operator, b: Operator): number => a.potential - b.potential,
-  "Favorite": (a: Operator, b: Operator): number => +a.favorite - +b.favorite,
-  "Modules": (a: Operator, b: Operator): number => a.module.reduce(r => r + 1, 0) - b.module.reduce(r => r + 1, 0),
-}
-
 interface Props {
+  sortKeys: string[]
   sortQueue: SortListItem[];
-  handleChange: (key: string, value?: (a: Operator, b: Operator) => number, desc?: boolean) => void;
+  handleChange: (key: string, desc?: boolean) => void;
 }
 
 const SortDialog = ((props: Props) => {
-  const { sortQueue, handleChange } = props;
+  const { sortKeys, sortQueue, handleChange } = props;
   const [open, setOpen] = React.useState(false);
 
   return (
@@ -34,14 +24,20 @@ const SortDialog = ((props: Props) => {
         <Tune fontSize="large" color="primary" />
       </IconButton>
       <Dialog open={open} onClose={() => setOpen(false)}>
-        <Button onClick={() => { handleChange("None", sortFunctions.None) }}>
+        <Button onClick={() => { handleChange("None", true)}}>
           +
         </Button>
         <FormControl>
           {sortQueue.map((sl: SortListItem, index: number) =>
-            <SortBlock value={sl.key} values={Object.keys(sortFunctions)} handleChange={handleChange} />
+            <SortBlock value={sl.key} values={sortKeys} handleChange={handleChange} />
           )}
         </FormControl>
+        <Button>
+          Cancel
+        </Button>
+        <Button >
+          Confirm
+        </Button>
       </Dialog>
     </>
   )
