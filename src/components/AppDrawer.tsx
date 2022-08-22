f74702bdd2630b445197c0a52798d027e2260a0f
Neia <neeia2718@gmail.com>
2022-08-21 21:56:47 -0700

    Add login and register buttons to app drawer

--- src/components/AppDrawer.tsx
+++ src/components/AppDrawer.tsx
@@ -1,9 +1,13 @@
-import { Person, ArrowRight, Description, ExpandLess, ExpandMore, PeopleAlt, PersonSearch, ContactPage, ManageAccounts } from "@mui/icons-material";
+﻿import { Person, ArrowRight, Description, ExpandLess, ExpandMore, PeopleAlt, PersonSearch, ContactPage, ManageAccounts } from "@mui/icons-material";
 import { Box, Button, Collapse, Divider, Drawer, Link, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Typography } from "@mui/material";
+import { getAuth, User } from "firebase/auth";
 import NextLink from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import config from "../data/config";
 import appTheme from "../styles/theme/appTheme";
+import initFirebase from "../util/initFirebase";
+import LoginButton from "./LoginButton";
+import RegisterButton from "./RegisterButton";
 
 
 const DRAWER_WIDTH_PX = 220;
@@ -13,12 +17,17 @@ const ICON_BY_PATH = [
   <PersonSearch key="c" />
 ];
 
+interface ConfigPage {
+  title: string;
+  description: string;
+  requireLogin?: boolean;
+}
+
 const ListItemTab: React.FC<{ tabTitle: string; startOpen: boolean; icon: React.ReactNode; children?: React.ReactNode }> = ({ tabTitle, startOpen, icon, children }) => {
   const [open, setOpen] = React.useState(startOpen);
   return (
     <>
       <ListItemButton
-        sx={{}}
         onClick={() => setOpen(!open)}>
         <ListItemIcon sx={{ minWidth: 0, pr: 1 }}>
           {icon}
@@ -46,6 +55,15 @@ const AppDrawer: React.FC<Props> = React.memo((props) => {
   const { title: currentTab, pages } = tabs[tab];
   const { title: currentPage } = pages[page as keyof typeof pages] ?? {};
 
+  initFirebase();
+  const [user, setUser] = useState<User | null>();
+  React.useEffect(() => {
+    setUser(getAuth().currentUser)
+  });
+
+  const [login, setLogin] = useState(false);
+  const [register, setRegister] = useState(false);
+
   const drawerContent = (
     <>
       <Typography
@@ -67,6 +85,20 @@ const AppDrawer: React.FC<Props> = React.memo((props) => {
         {/*<Offset />*/}
       </Typography>
       <Divider />
+      <Box sx={{ display: user ? "none" : "grid", gridTemplateColumns: "1fr 1fr", gap: "4px", m: "4px" }}>
+        <Button onClick={() => setLogin(true)}>
+          Log In
+        </Button>
+        <Button onClick={() => setRegister(true)}>
+          Register
+        </Button>
+        <LoginButton open={login} onClose={() => setLogin(false)} />
+        <RegisterButton open={register} onClose={() => setRegister(false)} />
+      </Box>
+      <Box sx={{ display: user ? "flex" : "none", m: "4px", pl: 2 }}>
+        お帰り, {user?.displayName}.
+      </Box>
+      <Divider />
       <List>
         {Object.entries(tabs).map(([tabPath, { title: tabTitle, pages }], i: number) => (
           <ListItemTab
@@ -75,40 +107,46 @@ const AppDrawer: React.FC<Props> = React.memo((props) => {
             startOpen={tabTitle === currentTab}
             icon={ICON_BY_PATH[i]}
           >
-            {Object.entries(pages).map(([pagePath, { title: pageTitle }]) => (
-              <ListItem key={pageTitle} sx={{ p: 0 }}>
-                <ListItemIcon sx={{ minWidth: 0, pr: 1, pl: 3, }}>
-                  <ArrowRight />
-                </ListItemIcon>
-                <ListItemButton sx={{
-                  ...(currentPage === pageTitle && {
-                    bgcolor: "primary.main",
-                    ":hover": {
-                      bgcolor: "primary.main",
-                      filter: "brightness(110%)"
-                    }
-                  })
-                }}>
-                  <NextLink
-                    href={`${tabPath}${pagePath}`}
-                    passHref
+            {Object.entries(pages)
+              .map(([pagePath, pg]: [string, ConfigPage]) => (
+                <ListItem key={pg.title} sx={{ p: 0 }}>
+                  <ListItemIcon sx={{ minWidth: 0, pr: 1, pl: 3, }}>
+                    <ArrowRight />
+                  </ListItemIcon>
+                  <ListItemButton
+                    sx={{
+                      ...(currentPage === pg.title && {
+                        bgcolor: "primary.main",
+                        ":hover": {
+                          bgcolor: "primary.main",
+                          filter: "brightness(110%)"
+                        }
+                      })
+                    }}
+                    disabled={pg.requireLogin && !user}
                   >
-                    <Link
-                      sx={{
-                        display: "block",
-                        width: "100%",
-                        py: 0.5,
-                        ...(currentPage === pageTitle && {
-                          color: "background.paper",
-                          fontWeight: "bold",
-                        })
-                      }} variant="body2">
-                      {pageTitle}
-                    </Link>
-                  </NextLink>
-                </ListItemButton>
-              </ListItem>
-            ))}
+                    <NextLink
+                      href={`${tabPath}${pagePath}`}
+                      passHref
+                    >
+                      <Link
+                        sx={{
+                          display: "block",
+                          width: "100%",
+                          py: 0.5,
+                          ...(currentPage === pg.title && {
+                            color: "background.paper",
+                            fontWeight: "bold",
+                          })
+                        }}
+                        variant="body2"
+                      >
+                        {pg.title}
+                      </Link>
+                    </NextLink>
+                  </ListItemButton>
+                </ListItem>
+              ))}
           </ListItemTab>
         ))}
       </List>
