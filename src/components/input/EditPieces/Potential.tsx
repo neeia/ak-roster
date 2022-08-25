51383c7204a26ef9993e7d684c78db5e179b8c58
Neia <neeia2718@gmail.com>
2022-08-25 03:51:21 -0700

    Replace border with underline

--- src/components/input/EditPieces/Potential.tsx
+++ src/components/input/EditPieces/Potential.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 import { Operator } from "../../../types/operator";
-import { Box, Button, IconButton } from "@mui/material";
+import { Box, IconButton } from "@mui/material";
 import { getMaxPotentialById } from "../../../util/changeOperator";
+
+const br = (op: string, pot: number) => {
+  const r = 4;
+  if (pot === 0) return `${r}px 0px 0px ${r}px`;
+  else if (pot === getMaxPotentialById(op) - 1) return `0px ${r}px ${r}px 0px`;
+  else return "0";
+}
 
 interface Props {
   op: Operator;
@@ -17,10 +24,10 @@ const Potential = ((props: Props) => {
     <Box sx={{
       display: "flex",
       justifyContent: "center",
-      gap: "4px",
     }}>
       {[...Array(getMaxPotentialById(op.id))].map((_, i) =>
         <IconButton
+          sx={{ borderRadius: br(op.id, i) }}
           className={op.potential === i + 1 ? "active" : "inactive"}
           onClick={() => onChange(op.id, "potential", i + 1)}
           disabled={!op.owned}
