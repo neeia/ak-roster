e2d1a63d6131d028cc2db9f052a7b8c7d2000775
Neia <neeia2718@gmail.com>
2022-08-26 00:53:33 -0700

    Move mui cache & theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import { CacheProvider } from '@emotion/react';
-import { ThemeProvider, CssBaseline } from '@mui/material';
-import createEmotionCache from '../util/createEmotionCache';
-import appTheme from '../styles/theme/appTheme';
 import '../styles/globals.css';
 import { AppProps } from 'next/app';
 
 
-const clientSideEmotionCache = createEmotionCache();
 
 const MyApp = (props: AppProps) => {
   const { Component, pageProps } = props;
 
   return (
-    <CacheProvider value={clientSideEmotionCache}>
-      <ThemeProvider theme={appTheme}>
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </CacheProvider>
+    <Component {...pageProps} />
   );
 };
 
