f42be518dc4a9757c1584346007d2895d5be44b6
Neia <neeia2718@gmail.com>
2022-08-26 00:53:33 -0700

    Move mui cache & theme

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { AppBar, Box, Container, IconButton, ThemeProvider, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, Container, CssBaseline, IconButton, ThemeProvider, Toolbar, Typography } from "@mui/material";
 import Head from "next/head";
 import config from "../data/config";
 import MenuIcon from '@mui/icons-material/Menu';
 import AppDrawer from "./AppDrawer";
 import appTheme from "../styles/theme/appTheme";
-
+import { CacheProvider } from '@emotion/react';
+import createEmotionCache from '../util/createEmotionCache';
 interface Props {
   tab: keyof typeof config.tabs;
   page: string;
@@ -16,89 +17,93 @@ const Layout = (props: Props) => {
   const { siteTitle, tabs } = config;
   const { title: tabTitle, description: tabDescription, pages } = tabs[tab];
   const { title: pageTitle, description: pageDescription } = pages[page as keyof typeof pages] ?? {};
-  const title = `${tabTitle} . ${pageTitle} . ${siteTitle}`
+  const title = `${pageTitle} - ${siteTitle}`
 
+  const clientSideEmotionCache = createEmotionCache();
   const [drawerOpen, setDrawerOpen] = React.useState(false);
   const handleDrawerToggle = React.useCallback(() => {
     setDrawerOpen((open) => !open);
   }, []);
 
   return (
-    <ThemeProvider theme={appTheme}>
-      <Head>
-        <title>{title}</title>
-        <meta
-          key="url"
-          property="og:url"
-          content={`${config.siteUrl}${page}`}
-        />
-        <meta key="title"
-          property="og:title"
-          content={pageTitle} />
-        <meta
-          key="description"
-          property="og:description"
-          content={pageDescription}
-        />
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#fbc02d" />
-      </Head>
-      <Box
-        display="grid"
-        height="100%"
-        gridTemplateAreas={'"drawer header" "drawer main"'}
-        gridTemplateRows="auto 1fr auto"
-        sx={{
-          gridTemplateColumns: {
-            xs: "0px 1fr",
-            xl: "220px 1fr"
-          }
-        }}
-      >
-        <AppDrawer
-          tab={tab}
-          page={page}
-          open={drawerOpen}
-          onDrawerToggle={handleDrawerToggle}
-        />
-        <AppBar
-          position="sticky"
-          enableColorOnDark
-          sx={{ gridArea: "header" }}
+    <CacheProvider value={clientSideEmotionCache}>
+      <ThemeProvider theme={appTheme}>
+        <CssBaseline />
+        <Head>
+          <title>{title}</title>
+          <meta
+            key="url"
+            property="og:url"
+            content={`${config.siteUrl}${page}`}
+          />
+          <meta key="title"
+            property="og:title"
+            content={pageTitle} />
+          <meta
+            key="description"
+            property="og:description"
+            content={pageDescription}
+          />
+          <link rel="manifest" href="/manifest.json" />
+          <meta name="theme-color" content="#fbc02d" />
+        </Head>
+        <Box
+          display="grid"
+          height="100%"
+          gridTemplateAreas={'"drawer header" "drawer main"'}
+          gridTemplateRows="auto 1fr auto"
+          sx={{
+            gridTemplateColumns: {
+              xs: "0px 1fr",
+              xl: "220px 1fr"
+            }
+          }}
         >
-          <Toolbar
-            variant="dense"
-            sx={{
-            }}
+          <AppDrawer
+            tab={tab}
+            page={page}
+            open={drawerOpen}
+            onDrawerToggle={handleDrawerToggle}
+          />
+          <AppBar
+            position="sticky"
+            enableColorOnDark
+            sx={{ gridArea: "header" }}
           >
-            <IconButton
-              aria-label="toggle navbar"
-              edge="start"
-              onClick={handleDrawerToggle}
+            <Toolbar
+              variant="dense"
               sx={{
-                display: {
-                  xl: "none",
-                },
               }}
             >
-              <MenuIcon sx={{ color: "background.paper" }} />
-            </IconButton>
-            <Box component="span" sx={{ verticalAlign: "bottom" }}>
-              <Typography component="h2" variant="h5" noWrap sx={{ display: "inline", verticalAlign: "baseline", }}>
-                {pageTitle}
-              </Typography>
-            </Box>
-          </Toolbar>
-        </AppBar>
-        <Container
-          component="main"
-          maxWidth="xl"
-          sx={{ gridArea: "main", p: 2, position: "relative" }}
-        >
-          {children}
-        </Container>
-      </Box>
-    </ThemeProvider>
+              <IconButton
+                aria-label="toggle navbar"
+                edge="start"
+                onClick={handleDrawerToggle}
+                sx={{
+                  display: {
+                    xl: "none",
+                  },
+                }}
+              >
+                <MenuIcon sx={{ color: "background.paper" }} />
+              </IconButton>
+              <Box component="span" sx={{ verticalAlign: "bottom" }}>
+                <Typography component="h2" variant="h5" noWrap sx={{ display: "inline", verticalAlign: "baseline", }}>
+                  {pageTitle}
+                </Typography>
+              </Box>
+            </Toolbar>
+          </AppBar>
+          <Container
+            component="main"
+            maxWidth="xl"
+            sx={{ gridArea: "main", p: 2, position: "relative" }}
+          >
+            {children}
+          </Container>
+        </Box>
+      </ThemeProvider>
+    </CacheProvider>
   )
 }
 export default Layout;
