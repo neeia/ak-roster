e2934da931f17fd64dd8e74486dbb4a8dc43fc19
Neia <neeia2718@gmail.com>
2022-08-26 00:52:56 -0700

    Skeleton commit

--- src/pages/network/lookup.tsx
+++ src/pages/network/lookup.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import type { NextPage } from "next";
-import { Box, Divider } from "@mui/material";
+import { Box, Divider, IconButton, InputAdornment, TextField } from "@mui/material";
 import Layout from "../../components/Layout";
 import initFirebase from "../../util/initFirebase";
 import { getUserStatus } from "../../util/getUserStatus";
@@ -11,73 +11,57 @@ import FriendID from "../../components/profile/FriendId";
 import Level from "../../components/profile/Level";
 import Server from "../../components/profile/Server";
 import Onboard from "../../components/profile/Onboard";
+import { Search } from "@mui/icons-material";
+import { child, get, getDatabase, ref } from "firebase/database";
+
 
 const Lookup: NextPage = () => {
   initFirebase();
+  const db = getDatabase();
 
-  const [user, setUser] = useState<User | null>();
-  useEffect(() => {
-    const auth = getAuth();
-    getUserStatus().then((user) => {
-      setUser(user);
+  const [username, setUsername] = useState<string>("");
+  const [uid, setUid] = useState<string>("");
+  const search = React.useCallback((e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    const checkUser = "phonebook/" + username.toLowerCase();
+    get(child(ref(db), checkUser)).then((snapshot) => {
+      if (snapshot.exists()) {
+        const checkUserData = "users/" + snapshot.val();
+        get(child(ref(db), checkUserData)).then((snapshot2) => {
+          if (snapshot2.exists()) {
+            console.log(snapshot2.val())
+          }
+        })
+      }
     })
-    onAuthStateChanged(auth, (user) => {
-      setUser(user);
-    });
-  }, []);
-
-  const [level, setLevel] = useState<string>("");
-  const [server, setServer] = useState<string>("");
-
+  }, [username]);
 
   return (
-    <Layout tab="/account" page="/profile">
-      {!user ? "" :
-        <Box sx={{
-          display: "flex",
-          flexDirection: "column",
-          gap: "1rem",
-          maxWidth: "sm",
-          justifySelf: "center",
-          "& .MuiButtonBase-root": {
-            boxShadow: 1,
-            backgroundColor: "info.main",
-          },
-          "& .inactive": {
-            opacity: 0.75,
-          },
-          "& .active": {
-            opacity: 1,
-            borderBottomWidth: "0.25rem",
-            borderBottomColor: "primary.main",
-            borderBottomStyle: "solid",
-            boxShadow: 2,
-            backgroundColor: "info.light",
-          },
-          "& .Mui-disabled": {
-            opacity: 0.25,
-            boxShadow: 0,
-          },
-          "& *:before": {
-            border: "none",
-            borderStyle: "none !important",
-          },
-          "& .MuiFilledInput-root": {
-            borderRadius: "2px",
-          },
-        }}>
-          <Box sx={{ display: "grid", gridTemplateColumns: "1fr auto", gap: 1 }}>
-            <FriendID user={user} />
-            <Server user={user} />
-          </Box>
-          <Box sx={{ display: "grid", gridTemplateColumns: "auto 1fr", gap: 1 }}>
-            <Level user={user} />
-            <Onboard user={user} />
-          </Box>
-          <Divider />
-          <Assistant user={user} />
-          <SupportSelection user={user} />
-        </Box>}
+    <Layout tab="/network" page="/lookup">
+      <Box sx={{
+        display: "flex",
+        maxWidth: "sm"
+      }}
+        component="form"
+      >
+        <TextField
+          sx={{ width: "100%" }}
+          autoFocus
+          autoComplete="off"
+          placeholder="Find a user..."
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton type="submit" onClick={search}>
+                  <Search />
+                </IconButton>
+              </InputAdornment>
+            )
+          }}
+        />
+      </Box>
     </Layout>
   );
 }
