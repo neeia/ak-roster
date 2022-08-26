6ec90170a8c3ee70beaea23e333882ab9559d7b5
Neia <neeia2718@gmail.com>
2022-08-25 21:49:18 -0700

    Initial commit

--- src/pages/network/lookup.tsx
+++ src/pages/network/lookup.tsx
@@ -1,35 +1,83 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import type { NextPage } from "next";
-import { Box } from "@mui/material";
-import dynamic from "next/dynamic";
+import { Box, Divider } from "@mui/material";
 import Layout from "../../components/Layout";
+import initFirebase from "../../util/initFirebase";
+import { getUserStatus } from "../../util/getUserStatus";
+import { getAuth, onAuthStateChanged, User } from "firebase/auth";
+import SupportSelection from "../../components/profile/SupportSelection";
+import Assistant from "../../components/profile/Assistant";
+import FriendID from "../../components/profile/FriendId";
+import Level from "../../components/profile/Level";
+import Server from "../../components/profile/Server";
+import Onboard from "../../components/profile/Onboard";
 
-const OperatorSelector = dynamic(
-  () => import("../../components/data/OperatorSelector"),
-  { ssr: false }
-);
-
-export enum SELECT_STATE {
-  Grid,
-  OpEdit,
-  PsEdit,
-  Batch
-}
-const EditOperator = dynamic(
-  () => import("../../components/data/EditOperator"),
-  { ssr: false }
-);
 const Lookup: NextPage = () => {
-  const [opId, setOpId] = React.useState("")
-  const [selectedPreset, setSelectedPreset] = useState("")
-  const [selectState, setSelectState] = React.useState(SELECT_STATE.Grid);
-  const [selectBatchOps, setSelectBatchOps] = useState<string[]>([])
+  initFirebase();
+
+  const [user, setUser] = useState<User | null>();
+  useEffect(() => {
+    const auth = getAuth();
+    getUserStatus().then((user) => {
+      setUser(user);
+    })
+    onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+  }, []);
+
+  const [level, setLevel] = useState<string>("");
+  const [server, setServer] = useState<string>("");
+
 
   return (
-    <Layout tab="/network" page="/lookup">
-      <Box>
-        Look for someone.
-      </Box>
+    <Layout tab="/account" page="/profile">
+      {!user ? "" :
+        <Box sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "1rem",
+          maxWidth: "sm",
+          justifySelf: "center",
+          "& .MuiButtonBase-root": {
+            boxShadow: 1,
+            backgroundColor: "info.main",
+          },
+          "& .inactive": {
+            opacity: 0.75,
+          },
+          "& .active": {
+            opacity: 1,
+            borderBottomWidth: "0.25rem",
+            borderBottomColor: "primary.main",
+            borderBottomStyle: "solid",
+            boxShadow: 2,
+            backgroundColor: "info.light",
+          },
+          "& .Mui-disabled": {
+            opacity: 0.25,
+            boxShadow: 0,
+          },
+          "& *:before": {
+            border: "none",
+            borderStyle: "none !important",
+          },
+          "& .MuiFilledInput-root": {
+            borderRadius: "2px",
+          },
+        }}>
+          <Box sx={{ display: "grid", gridTemplateColumns: "1fr auto", gap: 1 }}>
+            <FriendID user={user} />
+            <Server user={user} />
+          </Box>
+          <Box sx={{ display: "grid", gridTemplateColumns: "auto 1fr", gap: 1 }}>
+            <Level user={user} />
+            <Onboard user={user} />
+          </Box>
+          <Divider />
+          <Assistant user={user} />
+          <SupportSelection user={user} />
+        </Box>}
     </Layout>
   );
 }
