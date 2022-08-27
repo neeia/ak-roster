fdd2eb6f30c6a58512816f4060913c8d8d94737c
Neia <neeia2718@gmail.com>
2022-08-26 22:35:48 -0700

    Initial commit, placeholder

---
+++ src/pages/network/headhunt.tsx
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from "react";
+import type { NextPage } from "next";
+import { Box, Divider, IconButton, InputAdornment, TextField } from "@mui/material";
+import Layout from "../../components/Layout";
+import initFirebase from "../../util/initFirebase";
+import { getUserStatus } from "../../util/getUserStatus";
+import { getAuth, onAuthStateChanged, User } from "firebase/auth";
+import SupportSelection from "../../components/profile/SupportSelection";
+import Assistant from "../../components/profile/Assistant";
+import FriendID from "../../components/profile/FriendId";
+import Level from "../../components/profile/Level";
+import Server from "../../components/profile/Server";
+import Onboard from "../../components/profile/Onboard";
+import { Search } from "@mui/icons-material";
+import { child, get, getDatabase, ref } from "firebase/database";
+
+
+const Headhunt: NextPage = () => {
+  initFirebase();
+  const db = getDatabase();
+
+  const [username, setUsername] = useState<string>("");
+  const [uid, setUid] = useState<string>("");
+  const search = React.useCallback((e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    const checkUser = "phonebook/" + username.toLowerCase();
+    get(child(ref(db), checkUser)).then((snapshot) => {
+      if (snapshot.exists()) {
+        const checkUserData = "users/" + snapshot.val();
+        get(child(ref(db), checkUserData)).then((snapshot2) => {
+          if (snapshot2.exists()) {
+            console.log(snapshot2.val())
+          }
+        })
+      }
+    })
+  }, [db, username]);
+
+  return (
+    <Layout tab="/network" page="/headhunt">
+      <Box sx={{
+        display: "flex",
+        maxWidth: "sm"
+      }}
+        component="form"
+      >
+        <TextField
+          sx={{ width: "100%" }}
+          autoFocus
+          autoComplete="off"
+          placeholder="Find a user..."
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton type="submit" onClick={search}>
+                  <Search />
+                </IconButton>
+              </InputAdornment>
+            )
+          }}
+        />
+      </Box>
+    </Layout>
+  );
+}
+export default Headhunt;
