ce9e6131f65f5ddb97dfc85c7670d996345897f1
Neia <neeia2718@gmail.com>
2022-09-15 19:03:29 -0700

    Remove subpatch from patch notes

--- src/components/app/PatchNotes.tsx
+++ src/components/app/PatchNotes.tsx
@@ -35,7 +35,7 @@ const PatchNotes = () => {
               marginLeft: "8px",
               paddingTop: "12px",
             }}>
-            Patch 2.0.0
+            Patch 2.0
           </Typography>
           <IconButton onClick={() => setOpen(false)} sx={{ display: { sm: "none" } }}>
             <Close />
