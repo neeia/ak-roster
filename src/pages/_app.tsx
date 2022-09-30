b47ce901d5e267749c8e8f48a10033bb727407ca
Neia <neeia2718@gmail.com>
2022-09-30 02:46:56 -0700

    Move initfirebase to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
-import '../styles/globals.css';
+import 'styles/globals.css';
 import { AppProps } from 'next/app';
-import appTheme from "../styles/theme/appTheme";
-import createEmotionCache from '../util/createEmotionCache';
+import appTheme from "styles/theme/appTheme";
+import createEmotionCache from 'util/createEmotionCache';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { CacheProvider } from '@emotion/react';
+import { getApps, initializeApp } from 'firebase/app';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDjpt2G4GFQjYbPT5Mrj6L2meeWEnsCEgU",
+  authDomain: "ak-roster.firebaseapp.com",
+  projectId: "ak-roster",
+  storageBucket: "ak-roster.appspot.com",
+  messagingSenderId: "1076086810652",
+  appId: "1:1076086810652:web:ed1da74b87a08bf4b657d9",
+  measurementId: "G-VZXJ8MY6D1",
+  databaseURL: "https://ak-roster-default-rtdb.firebaseio.com/",
+};
 
 const MyApp = (props: AppProps) => {
   const { Component, pageProps } = props;
+
+  if (!getApps().length) initializeApp(firebaseConfig);
 
   const clientSideEmotionCache = createEmotionCache();
   return (
