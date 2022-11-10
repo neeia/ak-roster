d3662e917c0fe65b3db2666913ddd6e2367b502e
Neia <neeia2718@gmail.com>
2022-11-10 00:57:24 -0800

    Add tools section

---
+++ src/components/recruit/RecruitableOperatorChip.tsx
@@ -0,0 +1,52 @@
+import { Tooltip, Chip, Box, Typography, Divider } from "@mui/material";
+import Image from "next/image";
+import React from "react";
+import { RecruitableOperator } from "types/recruit";
+
+const RecruitableOperatorChip: React.FC<RecruitableOperator & { img?: string, alt?: string }> = React.memo(
+  (props) => {
+    const { id, tags, rarity, name, img, alt } = props;
+    return (
+      <>
+        <Tooltip
+          key="chipWrapper"
+          arrow
+          title={tags.join(", ")}
+          describeChild
+          placement="bottom"
+        >
+          <Chip
+            className={`rarity-${rarity}`}
+            avatar={
+              <Box ml={1} mr={-2}>
+                <Image
+                  src={`/img/avatars/${id}.png`}
+                  width={24}
+                  height={24}
+                  className="operator-avatar"
+                  alt=""
+                />
+              </Box>
+            }
+            label={
+              <Box sx={{ display: "flex", alignItems: "center", gap: 0.5, mr: -0.5 }}>
+                <Typography component="span">
+                  {name}
+                </Typography>
+                {img &&
+                  <Image
+                    src={img}
+                    width={24}
+                    height={24}
+                    alt={alt ?? ""}
+                  />
+                }
+              </Box>}
+          />
+        </Tooltip>
+      </>
+    );
+  }
+);
+RecruitableOperatorChip.displayName = "RecruitableOperatorChip";
+export default RecruitableOperatorChip;
