27c2f7f79ab8b41d70bba8d739c5e89021b0c459
Neia <neeia2718@gmail.com>
2023-02-28 05:15:43 -0800

    Update operators when complete goals

--- src/pages/planner/goals.tsx
+++ src/pages/planner/goals.tsx
@@ -9,7 +9,9 @@ import OperatorSearch from "components/planner/OperatorSearch";
 import { OpJsonObj } from "types/operator";
 import { useAppDispatch } from "store/hooks";
 import { addGoals } from "store/goalsSlice";
-import { PlannerGoal } from "types/goal";
+import { OperatorGoalCategory, PlannerGoal } from "types/goal";
+import useOperators from "util/useOperators";
+import operatorsJson from "data/operators.json";
 
 const MaterialsNeeded = dynamic(
   () => import("components/planner/MaterialsNeeded"),
@@ -22,11 +24,39 @@ const PlannerGoals = dynamic(() => import("components/planner/PlannerGoals"), {
 const Goals: NextPage = () => {
   const [operator, setOperator] = useState<OpJsonObj | null>(null);
   const dispatch = useAppDispatch();
+  const [operators, setOperators] = useOperators();
 
   const handleGoalsAdded = (newGoals: PlannerGoal[]) => {
     dispatch(addGoals(newGoals));
     setOperator(null);
   };
+
+  const handleGoalComplete = (goal: PlannerGoal) => {
+    setOperators((ops) => {
+      const op = ops[goal.operatorId];
+      const opData: OpJsonObj =
+        operatorsJson[goal.operatorId as keyof typeof operatorsJson];
+      switch (goal.category) {
+        case OperatorGoalCategory.Elite:
+          ops[goal.operatorId].promotion = Math.max(goal.eliteLevel, op.promotion);
+          break;
+        case OperatorGoalCategory.SkillLevel:
+          ops[goal.operatorId].skillLevel = Math.max(goal.skillLevel, op.skillLevel);
+          break;
+        case OperatorGoalCategory.Mastery:
+          const skillIndex = opData.skills.findIndex((sk) => sk.skillId === goal.skillId);
+          ops[goal.operatorId].mastery[skillIndex] = Math.max(goal.masteryLevel, op.mastery[skillIndex]);
+          break;
+        case OperatorGoalCategory.Module:
+          const moduleIndex = opData.modules.findIndex(
+            (m) => m.moduleId === goal.moduleId
+          )!;
+          ops[goal.operatorId].module[moduleIndex] = Math.max(goal.moduleLevel, op.mastery[moduleIndex]);
+          break;
+      }
+      return ops;
+    })
+  }
 
   return (
     <Layout tab="/planner" page="/goals">
@@ -58,7 +88,7 @@ const Goals: NextPage = () => {
             },
           }}
         >
-          <GoalSelect operator={operator} onGoalsAdded={handleGoalsAdded} />
+          <GoalSelect opData={operator && operators[operator.id]} operator={operator} onGoalsAdded={handleGoalsAdded} />
         </Grid>
       </Grid>
 
@@ -67,7 +97,7 @@ const Goals: NextPage = () => {
           <MaterialsNeeded />
         </Grid>
         <Grid item xs={12} lg={5}>
-          <PlannerGoals />
+          <PlannerGoals onCompleteGoal={handleGoalComplete} />
         </Grid>
       </Grid>
     </Layout>
