24bbc70c3c67abc8546b9846525d412cb521165c
yesod30 <samu.9301295@live.it>
2023-04-08 21:53:12 +0200

    Fixed link due to branch change

--- src/util/getMaterialFromImage.ts
+++ src/util/getMaterialFromImage.ts
@@ -10,7 +10,7 @@ export async function getMaterialsFromImage(fileList : File[]) :  Promise<Record
   const getSortId = (id : string) => items[id].sortId;
   const order = sortBy(Object.keys(items).filter(getSortId), getSortId);
   // replace the URL with a more stable link
-  const pkg = await fetch('https://raw.githubusercontent.com/yesod30/ak-roster/feature/depot-recognition/src/data/items.zip').then((res) => res.arrayBuffer())
+  const pkg = await fetch('https://raw.githubusercontent.com/yesod30/ak-roster/feature/data-import-export/src/data/items.zip').then((res) => res.arrayBuffer())
   const dr = new DeportRecognizer({ order, pkg});
   const result = [];
   for (let i = 0; i < fileList.length; i++) {
