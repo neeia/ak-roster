d52e51e93641bf33967c43483d8e4cb39683b23b
neia <51728998+neeia@users.noreply.github.com>
2023-05-17 14:56:43 -0700

    Redirect zip to main branch

--- src/util/getMaterialFromImage.ts
+++ src/util/getMaterialFromImage.ts
@@ -11,7 +11,7 @@ async function getMaterialsFromImage(fileList : File[]) :  Promise<Record<string
   const getSortId = (id : string) => items[id].sortId;
   const order = sortBy(Object.keys(items).filter(getSortId), getSortId);
   // replace the URL with a more stable link
-  const pkg = await fetch('https://raw.githubusercontent.com/yesod30/ak-roster/feature/data-import-export/src/data/items.zip').then((res) => res.arrayBuffer())
+  const pkg = await fetch('https://raw.githubusercontent.com/neeia/ak-roster/feature/data-import-export/src/data/items.zip').then((res) => res.arrayBuffer())
   const dr = new DeportRecognizer({ order, pkg});
   const result = [];
   for (let i = 0; i < fileList.length; i++) {
