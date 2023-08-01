c9fa9c882e78e88b3062490c7f58aa43b8e736ef
neia <51728998+neeia@users.noreply.github.com>
2023-08-01 13:11:54 -0700

    Remove sentry

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
-// This file sets a custom webpack configuration to use your Next.js app
-// with Sentry.
-// https://nextjs.org/docs/api-reference/next.config.js/introduction
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-const { withSentryConfig } = require('@sentry/nextjs');
-
-/* Modified as per https://melvingeorge.me/blog/nextjs-pwa */
-const withOffline = require("next-offline");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   //reactStrictMode: true,
-  productionBrowserSourceMaps: true,
   async redirects() {
     return [
       {
@@ -26,12 +16,4 @@ const nextConfig = {
   }
 }
 
-module.exports = nextConfig;
-module.exports = withSentryConfig(
-  module.exports,
-  {
-    dryRun: process.env.VERCEL_ENV !== "production"
-  },
-  { silent: true },
-  { hideSourcemaps: true },
-);
+export default nextConfig;
