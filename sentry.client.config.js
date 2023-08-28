8e23a5046087e3ca0d6cf4982154275df5d0f839
neia <51728998+neeia@users.noreply.github.com>
2023-08-28 03:45:58 -0700

    Update sentry sampling

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -9,7 +9,7 @@ const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 Sentry.init({
   dsn: SENTRY_DSN || 'https://e900c7bb12f84b4cadef1412f5820edd@o4504788728479744.ingest.sentry.io/4504788730380288',
   // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1.0,
+  tracesSampleRate: 0.01,
   // ...
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
