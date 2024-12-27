403c867b55fc23d5d3579f019c54f785a1979694
neia <51728998+neeia@users.noreply.github.com>
2024-12-26 21:13:49 -0800

    Simplified settings

--- src/components/app/PasswordTextField.tsx
+++ src/components/app/PasswordTextField.tsx
@@ -1,15 +1,14 @@
-import { VisibilityOffOutlined, VisibilityOutlined } from "@mui/icons-material";
-import { IconButton, InputAdornment, OutlinedTextFieldProps, TextField, Typography } from "@mui/material";
+import { Visibility, VisibilityOff, VisibilityOffOutlined, VisibilityOutlined } from "@mui/icons-material";
+import { Button, IconButton, InputAdornment, OutlinedTextFieldProps, TextField } from "@mui/material";
 import React, { useState } from "react";
 
 interface Props extends Omit<OutlinedTextFieldProps, "variant"> {
   value: string;
-  ariaId: string;
   variant?: "standard" | "filled" | "outlined";
 }
 
 const PasswordTextField = (props: Props) => {
-  const { label, value, onChange, ariaId, variant = "outlined", ...rest } = props;
+  const { label, value, onChange, variant = "outlined", sx, ...rest } = props;
 
   const [showPW, setShowPW] = useState<boolean>(false);
 
@@ -20,12 +19,26 @@ const PasswordTextField = (props: Props) => {
       onChange={onChange}
       type={showPW ? "" : "password"}
       variant={variant}
+      sx={{ display: "flex", ...sx }}
       InputProps={{
+        sx: { pr: 0 },
         endAdornment: (
-          <InputAdornment position="end">
-            <IconButton tabIndex={-1} onClick={() => setShowPW(!showPW)}>
-              {showPW ? <VisibilityOffOutlined fontSize="small" /> : <VisibilityOutlined fontSize="small" />}
-            </IconButton>
+          <InputAdornment
+            position="end"
+            sx={{
+              height: "100%",
+              maxHeight: "none",
+              borderLeft: "1px solid",
+              borderColor: "grey.600",
+            }}
+          >
+            <Button
+              variant="text"
+              onClick={() => setShowPW(!showPW)}
+              sx={{ height: "100%", aspectRatio: "1 / 1", "&:not(:hover), &:not(:focus)": { opacity: 0.75 } }}
+            >
+              {showPW ? <VisibilityOff fontSize="small" /> : <Visibility fontSize="small" />}
+            </Button>
           </InputAdornment>
         ),
       }}
--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,15 +1,15 @@
-﻿import React, { useState } from "react";
+﻿import React, { useContext, useState } from "react";
 import type { NextPage } from "next";
-import { Box, Button, Divider, IconButton, InputAdornment, TextField, Typography } from "@mui/material";
+import { AlertProps, Box, Button, Divider, TextField, Typography } from "@mui/material";
 import Layout from "components/Layout";
-import UpdateEmail from "components/settings/UpdateEmail";
-import UpdatePassword from "components/settings/UpdatePassword";
 import useAccount from "util/hooks/useAccount";
-import UpdatePrivacy from "components/settings/UpdatePrivacy";
 import { ContentPasteOutlined, InventoryOutlined } from "@mui/icons-material";
 import { enqueueSnackbar } from "notistack";
 import supabase from "supabase/supabaseClient";
 import handlePostgrestError from "util/fns/handlePostgrestError";
+import { UserContext } from "./_app";
+import PasswordTextField from "components/app/PasswordTextField";
+import ResetPassword from "components/app/ResetPassword";
 
 function isValidUsername(input: string) {
   const regex = /^[a-zA-Z0-9_-]+$/;
@@ -19,20 +19,23 @@ function isValidUsername(input: string) {
 const Settings: NextPage = () => {
   const [account, setAccount, { loading }] = useAccount();
 
-  const error = (message: string) => enqueueSnackbar(message, { variant: "error" });
+  const alert = (message: string, variant?: "default" | "error" | "success" | "warning" | "info") =>
+    enqueueSnackbar(message, { variant: variant || "error" });
 
   const [username, setUsername] = useState("");
   const [display_name, setDisplayName] = useState("");
   const [copyLink, setCopiedLink] = useState(false);
 
+  const user = useContext(UserContext);
+
   const updateUsername = async () => {
     if (!username || !account) return;
     if (username.length > 32) {
-      error("Use a maximum of 32 characters.");
+      alert("Use a maximum of 32 characters.");
       return;
     }
     if (!isValidUsername(username)) {
-      error("Chosen username has invalid characters.");
+      alert("Chosen username has invalid characters.");
       return;
     }
     const { count, error: sberror } = await supabase
@@ -43,7 +46,7 @@ const Settings: NextPage = () => {
 
     if (count == null) return;
     if (count) {
-      error("That username is taken.");
+      alert("That username is taken.");
       return;
     }
     const dn = account.display_name;
@@ -58,12 +61,34 @@ const Settings: NextPage = () => {
   const updateDisplayName = async () => {
     if (!display_name || !account) return;
     if (display_name.length > 32) {
-      error("Use a maximum of 32 characters.");
+      alert("Use a maximum of 32 characters.");
       return;
     }
 
     setAccount({ ...account, display_name: username });
   };
+
+  const [email, setEmail] = useState("");
+
+  async function updateEmail() {
+    const { error } = await supabase.auth.updateUser({ email: email });
+    if (error) alert(`AUTH${error.code}: ${error.message}`);
+    else alert("Check your new e-mail to confirm the e-mail change.");
+  }
+
+  const [password1, setNewPassword] = useState<string>("");
+  const [password2, setRepeatPassword] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const [resetOpen, setResetOpen] = useState<boolean>(false);
+
+  async function changePassword() {
+    const { error } = await supabase.auth.updateUser({
+      password: password1,
+    });
+    if (error) alert(`AUTH${error.code}: ${error.message}`);
+    else alert("Password changed.");
+  }
 
   return (
     <Layout tab="" page="/settings">
@@ -74,7 +99,7 @@ const Settings: NextPage = () => {
           sx={{
             display: "flex",
             flexDirection: "column",
-            gap: "12px 6px",
+            gap: 2,
             maxWidth: "sm",
             "& *:before": {
               border: "none",
@@ -83,79 +108,131 @@ const Settings: NextPage = () => {
             "& .MuiFilledInput-root": {
               borderRadius: "4px",
             },
-            "& > .MuiButton-root": {
+            "& > * > .MuiButton-root": {
               width: "fit-content",
             },
           }}
         >
-          <TextField
-            label="Share Link"
-            value={account.username ? `https://www.krooster.com/u/${account.username}` : "---"}
-            variant="standard"
-            disabled
-            InputProps={{
-              endAdornment: (
-                <InputAdornment position="end">
-                  <Typography id="copy-label">{copyLink ? "Copied!" : "Copy"}</Typography>
-                  <IconButton
-                    aria-labelledby="copy-label"
-                    onClick={() => {
-                      setCopiedLink(true);
-                      navigator.clipboard.writeText(`https://krooster.com/u/${account.display_name}`);
-                    }}
-                  >
-                    {copyLink ? <InventoryOutlined height="1rem" /> : <ContentPasteOutlined height="1rem" />}
-                  </IconButton>
-                </InputAdornment>
-              ),
-            }}
-          />
-          <Divider />
-          Change Username
-          <TextField
-            label="Current Username"
-            value={account.username ?? "No Username Set"}
-            variant="standard"
-            disabled
-          />
-          <TextField
-            label="New Username"
-            value={username}
-            onChange={(e) => {
-              setUsername(e.target.value.toLocaleLowerCase());
-            }}
-            helperText="Valid Characters: [a-z], [0-9], underscore (_), and hyphen (-). Max length of 32."
-            variant="filled"
-          />
-          <Button onClick={updateUsername} disabled={!username || username === account.username}>
-            Change Username
-          </Button>
-          <Divider />
-          Change Display Name
-          <TextField
-            label="Current Display Name"
-            value={account.display_name || "No Display Name Set"}
-            variant="standard"
-            disabled
-          />
-          <TextField
-            label="New Display Name"
-            value={display_name}
-            onChange={(e) => {
-              setDisplayName(e.target.value);
-            }}
-            variant="filled"
-          />
           <Button
-            onClick={updateDisplayName}
-            disabled={!display_name || display_name === account.display_name || display_name.length > 32}
+            sx={{ display: "flex", width: "100%" }}
+            onClick={() => {
+              setCopiedLink(true);
+              navigator.clipboard.writeText(`https://krooster.com/u/${account.display_name}`);
+            }}
           >
-            Change Display Name
+            <TextField
+              label="Share Link"
+              value={account.username ? `https://www.krooster.com/u/${account.username}` : "---"}
+              variant="standard"
+              disabled
+              fullWidth
+              sx={{ pointerEvents: "none" }}
+            />
+            <Typography sx={{ mr: 1 }}>{copyLink ? "Copied!" : "Copy"}</Typography>
+            {copyLink ? <InventoryOutlined height="1rem" /> : <ContentPasteOutlined height="1rem" />}
           </Button>
           <Divider />
-          <UpdateEmail />
+          <Box sx={{ display: "flex", flexDirection: "column", gap: 1 }}>
+            <Typography variant="h2" sx={{ fontSize: "18px" }}>
+              Change Username
+            </Typography>
+            <TextField
+              label="Current Username"
+              value={account.username ?? "No Username Set"}
+              variant="standard"
+              disabled
+            />
+            <TextField
+              label="New Username"
+              value={username}
+              onChange={(e) => {
+                setUsername(e.target.value.toLocaleLowerCase());
+              }}
+              helperText="Valid Characters: [a-z], [0-9], underscore (_), and hyphen (-). Max length of 32."
+              variant="filled"
+            />
+            <Button onClick={updateUsername} disabled={!username || username === account.username}>
+              Change Username
+            </Button>
+          </Box>
           <Divider />
-          <UpdatePassword user={account} />
+          <Box sx={{ display: "flex", flexDirection: "column", gap: 1 }}>
+            <Typography variant="h2" sx={{ fontSize: "18px" }}>
+              Change Display Name
+            </Typography>
+            <TextField
+              label="Current Display Name"
+              value={account.display_name || "No Display Name Set"}
+              variant="standard"
+              disabled
+            />
+            <TextField
+              label="New Display Name"
+              value={display_name}
+              onChange={(e) => {
+                setDisplayName(e.target.value);
+              }}
+              variant="filled"
+            />
+            <Button
+              onClick={updateDisplayName}
+              disabled={!display_name || display_name === account.display_name || display_name.length > 32}
+            >
+              Change Display Name
+            </Button>
+          </Box>
+          <Divider />
+          <Box sx={{ display: "flex", flexDirection: "column", gap: 1 }}>
+            <Typography variant="h2" sx={{ fontSize: "18px" }}>
+              Change Email
+            </Typography>
+            <TextField label="Current Email" value={user?.email} variant="standard" disabled />
+            <TextField
+              label="New Email"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+              variant="filled"
+            />
+            <Button onClick={updateEmail} disabled={!email}>
+              Change Email
+            </Button>
+          </Box>
+          <Divider />
+          <Box sx={{ display: "flex", flexDirection: "column", gap: 1 }}>
+            <Typography variant="h2" sx={{ fontSize: "18px" }}>
+              Change Password
+            </Typography>
+            <PasswordTextField
+              label="Current Password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+              variant="filled"
+            />
+            <PasswordTextField
+              label="New Password"
+              value={password1}
+              onChange={(e) => {
+                setNewPassword(e.target.value);
+              }}
+              variant="filled"
+            />
+            <PasswordTextField
+              label="Repeat Password"
+              value={password2}
+              onChange={(e) => {
+                setRepeatPassword(e.target.value);
+              }}
+              error={password1 !== password2}
+              variant="filled"
+            />
+            <Button onClick={changePassword}>Change Password</Button>
+            <Button onClick={() => setResetOpen(true)}>Forgot Password?</Button>
+          </Box>
+          <ResetPassword open={resetOpen} onClose={() => setResetOpen(false)} />
         </Box>
       )}
     </Layout>
