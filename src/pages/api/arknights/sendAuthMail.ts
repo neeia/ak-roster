1ee4c99d09ac7ac42218a0dd4ceffc2c50a02954
neia <51728998+neeia@users.noreply.github.com>
2025-01-07 00:27:14 -0800

    Replace space with plus in mail api

--- src/pages/api/arknights/sendAuthMail.ts
+++ src/pages/api/arknights/sendAuthMail.ts
@@ -3,6 +3,6 @@ import { sendTokenToMail } from "util/hgApi/yostarAuth";
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
   const { mail } = req.query as { mail: string };
-  const success = await sendTokenToMail(mail, "en");
+  const success = await sendTokenToMail(mail.replaceAll(" ", "+"), "en");
   res.status(success ? 200 : 500);
 }
