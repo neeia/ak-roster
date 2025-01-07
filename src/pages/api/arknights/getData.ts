cf61929bc898467fedeea1c0b95d18634bf3696e
neia <51728998+neeia@users.noreply.github.com>
2025-01-07 00:39:22 -0800

    fix url encoding for getdata as well...

--- src/pages/api/arknights/getData.ts
+++ src/pages/api/arknights/getData.ts
@@ -5,7 +5,7 @@ import { TokenData } from "types/arknightsApiTypes/apiTypes";
 export default async function (req: NextApiRequest, res: NextApiResponse) {
   const { mail, code } = req.query as { mail: string; code: string };
   const token = req.body as TokenData;
-  const data = token ? await getGameDataWithToken(token) : await getGameData(mail, code);
+  const data = token ? await getGameDataWithToken(token) : await getGameData(mail.replaceAll(" ", "+"), code);
   if (data) {
     res.status(200).json(data);
   } else {
