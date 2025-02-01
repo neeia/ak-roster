450641dad70302e632e5d99e74a85d664b4fca43
Voiddp <voiddp@gmail.com>
2025-02-01 12:29:34 +0000

    fix chips pair craft loop, tweak all dualchips as craftable equally.

--- src/util/fns/depot/canCompleteByCrafting.ts
+++ src/util/fns/depot/canCompleteByCrafting.ts
@@ -7,6 +7,11 @@ const canCompleteByCrafting = (
   depot: Record<string, DepotItem>,
   crafting: string[]
 ) => {
+  //0. Tweak: all Dualchips => hypothetically craftable
+  //Needed to showcase depot for All Goals better
+  //variables to save/refund/consume catalists
+  const catalistId = "32001";
+  let catalistsUsedQuantity = 0;
 
   // 1. Save original recipe
   const _materialsNeeded = { ...materialsNeeded };
@@ -27,21 +32,32 @@ const canCompleteByCrafting = (
           if (ingredients != null) {
             const multiplier = Math.ceil(remaining / (itemYield ?? 1));
             ingredients.forEach((ingr) => {
-              ingredientToCraftedItemsMapping[ingr.id] = [...(ingredientToCraftedItemsMapping[ingr.id] ?? []), item.id];
+              //Fix chips-pairs loop: add <" Chip"> or <" Chip" Pack> as ingredient only if craftedItem Chip isn't already ingredient
+              if (itemBeingCrafted.name.includes(" Chip")
+                && !Boolean(ingredientToCraftedItemsMapping[item.id] ?? false)) {
+                ingredientToCraftedItemsMapping[ingr.id] = [...(ingredientToCraftedItemsMapping[ingr.id] ?? []), item.id];
+              }
               materialsNeeded[ingr.id] = (materialsNeeded[ingr.id] ?? 0) + ingr.quantity * multiplier;
             });
           }
         }
       }
     });
-
+  
   // 3. calculate what ingredients can be fulfilled by crafting and craft them in _depot
   const _depot = { ...depot }; // need to hypothetically deduct from stock
   const craftableItems: Record<string, boolean> = {};
   Object.keys(materialsNeeded)
     .filter(
       (craftedItemId) =>
-        (materialsNeeded[craftedItemId] - (depot[craftedItemId]?.stock ?? 0)) > 0)
+        (
+          (materialsNeeded[craftedItemId] - (depot[craftedItemId]?.stock ?? 0)) > 0)
+        //only craft "Crafting" items
+        && crafting.includes(craftedItemId)
+        //Fix chips-pairs loop: only craft Chips that are not ingredients (of other chips)
+        && (!itemsJson[craftedItemId as keyof typeof itemsJson].name.includes(" Chip")//  chip   = 0 not-chip  = 1
+          || !Boolean(ingredientToCraftedItemsMapping[craftedItemId]))                //  inr    = 0 not-inr   = 1 
+    )
     .sort(
       (itemA, itemB) =>
         itemsJson[itemA as keyof typeof itemsJson].tier - itemsJson[itemB as keyof typeof itemsJson].tier
@@ -61,10 +77,17 @@ const canCompleteByCrafting = (
         // now deduct from crafting supply
         ingredients.forEach((ingr) => {
           const copy = { ..._depot[ingr.id] };
+          const refund = copy.stock;
           copy.stock = Math.max((_depot[ingr.id]?.stock ?? 0) - ingr.quantity * numTimesToCraft);
+          //Tweak: all Dualchips => hypothetically craftable: refund catalists
+          if ((craftedItem.name.includes("Dualchip")
+            && ingr.id === catalistId)) {
+              copy.stock = refund;
+              catalistsUsedQuantity = ingr.quantity * numTimesToCraft;
+            }
           _depot[ingr.id] = copy;
         });
-        if (shortage - numTimesToCraft <= 0) {
+        if ((shortage - itemYield * numTimesToCraft <= 0)) {
           craftableItems[craftedItemId] = true;
         }
         // even if the crafted item can't be completed, update our hypothetical depot counts
@@ -74,18 +97,28 @@ const canCompleteByCrafting = (
       }
     });
 
-  Object.keys(ingredientToCraftedItemsMapping).forEach((ingrId) => {
-    if ((materialsNeeded[ingrId] ?? 0) - (_depot[ingrId]?.stock ?? 0) <= 0) {
-      craftableItems[ingrId] = true;
-    }
-  });
+  Object.keys(ingredientToCraftedItemsMapping)
+    //Fix chips-pairs loop: chips that are ingredients (of other chips) - can't be auto set to cratable
+    .filter(ingrId => !itemsJson[ingrId as keyof typeof itemsJson].name.includes(" Chip"))
+    .forEach((ingrId) => {
+      if ((materialsNeeded[ingrId] ?? 0) - (_depot[ingrId]?.stock ?? 0) <= 0) {
+        craftableItems[ingrId] = true;
+      }
+    });
 
-  // 4. Use up original recipe materials, "theoretically crafted" in #3 and already existed
+  // 4. calculate Goal Completion _depot
+  // 4.1 Consume original recipe materials, both hypothetically crafted in #3 and stock  
   Object.keys(_materialsNeeded).forEach((ingrId) => {
     const copy = { ..._depot[ingrId] };
     copy.stock = Math.max((_depot[ingrId]?.stock ?? 0) - _materialsNeeded[ingrId], 0);
     _depot[ingrId] = copy;
   });
+  // 4.2 Tweak: all Dualchips => hypothetically craftable: Consume used catalists
+  if (catalistsUsedQuantity > 0) {
+    const copy = { ..._depot[catalistId] };
+    copy.stock = Math.max((_depot[catalistId]?.stock ?? 0) - catalistsUsedQuantity,0);
+    _depot[catalistId] = copy;
+  }
 
   return { craftableItems, ingredientToCraftedItemsMapping, depot: _depot };
 };
