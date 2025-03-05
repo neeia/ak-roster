1cbe476e7b614e8b4404fcc74493e912e1fd3f2b
neia <51728998+neeia@users.noreply.github.com>
2025-03-05 01:29:22 -0800

    change max shadow to  rarity

--- src/components/data/OperatorBlock.tsx
+++ src/components/data/OperatorBlock.tsx
@@ -47,7 +47,7 @@ const OperatorBlock = (props: Props) => {
         display: "flex",
         flexDirection: "column",
         backgroundColor: "background.light",
-        boxShadow: maxed ? (theme) => `0px 0px 8px ${theme.palette.primary.main}` : 1,
+        boxShadow: maxed ? `0px 0px 8px ${rarityColors[op.rarity]}` : 1,
         padding: { xs: "4px 8px 4px 6px", sm: "6px 10px 8px 10px" },
         margin: { xs: "2px 4px 4px 10px", sm: "4px 12px 12px 12px" },
         borderRadius: "4px",
@@ -118,197 +118,217 @@ const OperatorBlock = (props: Props) => {
         }}
       >
         <Image src={getAvatar(op)} fill sizes="(max-width: 768px) 80px, 120px" alt="" />
+        {maxed && (
+          <Box
+            sx={{
+              position: "absolute",
+              left: 0,
+              bottom: -4,
+              px: 0.5,
+              backgroundColor: rarityColors[op.rarity],
+              color: "#121212",
+              borderRadius: "4px 4px 0px 0px",
+              fontSize: { xs: 12, sm: 14 },
+            }}
+          >
+            Maxed
+          </Box>
+        )}
       </Box>
-      {/* Potential, Promotion, Level */}
-      <Box
-        sx={{
-          position: "absolute",
-          left: -12,
-          bottom: -8,
-          display: "flex",
-          flexDirection: "column",
-          gap: "2px",
-          zIndex: 1,
-        }}
-      >
-        {op.potential > 1 ? (
+      {!maxed && (
+        <>
+          {/* Potential, Promotion, Level */}
           <Box
             sx={{
-              position: "relative",
-              width: { xs: "12px", sm: "20px" },
-              height: { xs: "16px", sm: "24px" },
-              ml: "4px",
-              marginBottom: { xs: "2px", sm: "8px" },
-            }}
-          >
-            <Box
-              sx={{
-                width: "100%",
-                height: "100%",
-                position: "absolute",
-                m: "auto",
-                left: -4,
-                right: -4,
-                top: -4,
-                bottom: -4,
-                backgroundImage: "url('/img/rank/bg.png')",
-                backgroundSize: "100% 100%",
-              }}
-            >
-              <Image
-                src={`/img/potential/${op.potential}.png`}
-                fill
-                sizes={iconSizes}
-                alt={`Potential ${op.potential}`}
-              />
-            </Box>
-          </Box>
-        ) : null}
-        {op.elite > 0 ? (
-          <Box
-            sx={{
-              width: { xs: "20px", sm: "32px" },
-              height: { xs: "20px", sm: "32px" },
-              position: "relative",
-              marginBottom: { xs: "0px", sm: "4px" },
-            }}
-          >
-            <Image
-              src={`/img/elite/${op.elite}_s_box.png`}
-              fill
-              sizes="(max-width: 768px) 20px, 32px"
-              alt={`Elite ${op.elite}`}
-            />
-          </Box>
-        ) : null}
-        {op.elite > 0 || op.level > 1 ? (
-          <Box
-            sx={{
-              height: { xs: "32px", sm: "48px" },
-              aspectRatio: "1 / 1",
-              fontSize: { xs: "18px", sm: "24px" },
-              lineHeight: 1,
+              position: "absolute",
+              left: -12,
+              bottom: -8,
               display: "flex",
               flexDirection: "column",
-              alignItems: "center",
-              justifyContent: "center",
-              borderRadius: "50%",
-              backgroundColor: "background.default",
-              border: "2px solid",
-              borderColor: op.level === MAX_LEVEL_BY_RARITY[op.rarity][2] ? "primary.light" : "grey.500",
+              gap: "2px",
+              zIndex: 1,
             }}
           >
-            <Box
-              component="abbr"
-              title="Level"
-              sx={{
-                fontSize: "9px",
-                lineHeight: "4px",
-                display: {
-                  xs: "none",
-                  sm: "flex",
-                },
-                textDecoration: "none",
-              }}
-            >
-              LV
-            </Box>
-            {op.level}
-          </Box>
-        ) : null}
-      </Box>
-      {/* Skills */}
-      <Box
-        sx={{
-          position: "absolute",
-          top: 32,
-          right: { xs: "0px", sm: "-12px" },
-          display: op.skill_level > 1 ? "flex" : "none",
-          flexDirection: "column",
-          gap: "2px",
-        }}
-      >
-        {[...Array(op.skillData?.length ?? 0)].map((_, i: number) => (
-          <Box
-            key={i}
-            sx={{
-              display: op.elite >= i ? "grid" : "none",
-              marginLeft: { xs: "0px", sm: `${4 * i}px` },
-              width: { xs: "16px", sm: "24px" },
-              height: { xs: "16px", sm: "24px" },
-              position: "relative",
-              backgroundImage: "url('/img/rank/bg.png')",
-              backgroundSize: "contain",
-            }}
-          >
-            {!op.masteries[i] ? (
-              <Image
-                src={`/img/rank/${op.skill_level}.png`}
-                fill
-                sizes={iconSizes}
-                alt={`Skill ${i + 1} Rank ${op.skill_level}`}
-              />
-            ) : (
-              <Image
-                src={`/img/rank/m-${op.masteries[i]}.png`}
-                fill
-                sizes={iconSizes}
-                alt={`Mastery ${op.masteries[i]}`}
-              />
-            )}
-          </Box>
-        ))}
-      </Box>
-      {/* Modules */}
-      <Box
-        sx={{
-          position: "absolute",
-          right: -8,
-          bottom: -12,
-          display: "flex",
-          flexDirection: "row-reverse",
-          alignSelf: "end",
-          gap: "4px",
-        }}
-      >
-        {(op.moduleData ?? [])
-          .filter((mod) => op.modules[mod.moduleId] > 0)
-          .map((mod, i) => (
-            <Box
-              key={mod.moduleId}
-              sx={{
-                position: "relative",
-                display: "grid",
-                height: { xs: "24px", sm: "32px" },
-                aspectRatio: "1 / 1",
-                backgroundColor: "background.default",
-                backgroundImage: "url('/img/rank/bg.png')",
-                backgroundSize: "100% 100%",
-              }}
-            >
-              <Image src={getModUrl(mod)} fill sizes="(max-width: 768px) 24px, 32px" alt={mod.typeName} />
-              <Typography
-                zIndex={1}
-                component="abbr"
-                title={`Stage ${op.modules[mod.moduleId]}`}
+            {op.potential > 1 ? (
+              <Box
                 sx={{
-                  fontSize: { xs: "10px", sm: "12px" },
-                  position: "absolute",
-                  width: "min-content !important",
-                  lineHeight: 1,
-                  textDecoration: "none",
-                  backgroundColor: "grey.950",
-                  pl: "2px",
-                  right: -1,
-                  bottom: -1,
+                  position: "relative",
+                  width: { xs: "12px", sm: "20px" },
+                  height: { xs: "16px", sm: "24px" },
+                  ml: "4px",
+                  marginBottom: { xs: "2px", sm: "8px" },
                 }}
               >
-                {mod.typeName.slice(-1)}
-                {op.modules[mod.moduleId] > 1 && op.modules[mod.moduleId]}
-              </Typography>
-            </Box>
-          ))}
-      </Box>
+                <Box
+                  sx={{
+                    width: "100%",
+                    height: "100%",
+                    position: "absolute",
+                    m: "auto",
+                    left: -4,
+                    right: -4,
+                    top: -4,
+                    bottom: -4,
+                    backgroundImage: "url('/img/rank/bg.png')",
+                    backgroundSize: "100% 100%",
+                  }}
+                >
+                  <Image
+                    src={`/img/potential/${op.potential}.png`}
+                    fill
+                    sizes={iconSizes}
+                    alt={`Potential ${op.potential}`}
+                  />
+                </Box>
+              </Box>
+            ) : null}
+            {op.elite > 0 ? (
+              <Box
+                sx={{
+                  width: { xs: "20px", sm: "32px" },
+                  height: { xs: "20px", sm: "32px" },
+                  position: "relative",
+                  marginBottom: { xs: "0px", sm: "4px" },
+                }}
+              >
+                <Image
+                  src={`/img/elite/${op.elite}_s_box.png`}
+                  fill
+                  sizes="(max-width: 768px) 20px, 32px"
+                  alt={`Elite ${op.elite}`}
+                />
+              </Box>
+            ) : null}
+            {op.elite > 0 || op.level > 1 ? (
+              <Box
+                sx={{
+                  height: { xs: "32px", sm: "48px" },
+                  aspectRatio: "1 / 1",
+                  fontSize: { xs: "18px", sm: "24px" },
+                  lineHeight: 1,
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  borderRadius: "50%",
+                  backgroundColor: "background.default",
+                  border: "2px solid",
+                  borderColor: op.level === MAX_LEVEL_BY_RARITY[op.rarity][2] ? "primary.light" : "grey.500",
+                }}
+              >
+                <Box
+                  component="abbr"
+                  title="Level"
+                  sx={{
+                    fontSize: "9px",
+                    lineHeight: "4px",
+                    display: {
+                      xs: "none",
+                      sm: "flex",
+                    },
+                    textDecoration: "none",
+                  }}
+                >
+                  LV
+                </Box>
+                {op.level}
+              </Box>
+            ) : null}
+          </Box>
+          {/* Skills */}
+          <Box
+            sx={{
+              position: "absolute",
+              top: 32,
+              right: { xs: "0px", sm: "-12px" },
+              display: op.skill_level > 1 ? "flex" : "none",
+              flexDirection: "column",
+              gap: "2px",
+            }}
+          >
+            {[...Array(op.skillData?.length ?? 0)].map((_, i: number) => (
+              <Box
+                key={i}
+                sx={{
+                  display: op.elite >= i ? "grid" : "none",
+                  marginLeft: { xs: "0px", sm: `${4 * i}px` },
+                  width: { xs: "16px", sm: "24px" },
+                  height: { xs: "16px", sm: "24px" },
+                  position: "relative",
+                  backgroundImage: "url('/img/rank/bg.png')",
+                  backgroundSize: "contain",
+                }}
+              >
+                {!op.masteries[i] ? (
+                  <Image
+                    src={`/img/rank/${op.skill_level}.png`}
+                    fill
+                    sizes={iconSizes}
+                    alt={`Skill ${i + 1} Rank ${op.skill_level}`}
+                  />
+                ) : (
+                  <Image
+                    src={`/img/rank/m-${op.masteries[i]}.png`}
+                    fill
+                    sizes={iconSizes}
+                    alt={`Mastery ${op.masteries[i]}`}
+                  />
+                )}
+              </Box>
+            ))}
+          </Box>
+          {/* Modules */}
+          <Box
+            sx={{
+              position: "absolute",
+              right: -8,
+              bottom: -12,
+              display: "flex",
+              flexDirection: "row-reverse",
+              alignSelf: "end",
+              gap: "4px",
+            }}
+          >
+            {(op.moduleData ?? [])
+              .filter((mod) => op.modules[mod.moduleId] > 0)
+              .map((mod, i) => (
+                <Box
+                  key={mod.moduleId}
+                  sx={{
+                    position: "relative",
+                    display: "grid",
+                    height: { xs: "24px", sm: "32px" },
+                    aspectRatio: "1 / 1",
+                    backgroundColor: "background.default",
+                    backgroundImage: "url('/img/rank/bg.png')",
+                    backgroundSize: "100% 100%",
+                  }}
+                >
+                  <Image src={getModUrl(mod)} fill sizes="(max-width: 768px) 24px, 32px" alt={mod.typeName} />
+                  <Typography
+                    zIndex={1}
+                    component="abbr"
+                    title={`Stage ${op.modules[mod.moduleId]}`}
+                    sx={{
+                      fontSize: { xs: "10px", sm: "12px" },
+                      position: "absolute",
+                      width: "min-content !important",
+                      lineHeight: 1,
+                      textDecoration: "none",
+                      backgroundColor: "grey.950",
+                      pl: "2px",
+                      right: -1,
+                      bottom: -1,
+                    }}
+                  >
+                    {mod.typeName.slice(-1)}
+                    {op.modules[mod.moduleId] > 1 && op.modules[mod.moduleId]}
+                  </Typography>
+                </Box>
+              ))}
+          </Box>
+        </>
+      )}
     </Box>
   );
 };
