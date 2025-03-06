59f8af3ef427c05318735f793b71dfb84fffa5f5
Voiddp <voiddp@gmail.com>
2025-03-06 16:04:11 +0000

    added summary dialog
    summary as button
    
    added statistic into summary

--- src/components/planner/depot/MaterialsNeeded.tsx
+++ src/components/planner/depot/MaterialsNeeded.tsx
@@ -12,6 +12,7 @@ import ItemNeeded from "./ItemNeeded";
 import getGoalIngredients from "util/fns/depot/getGoalIngredients";
 import DepotItem from "types/depotItem";
 import ExportImportDialog from "./ExportImportDialog";
+import MaterialsSummaryDialog from "./MaterialsSummaryDialog";
 import Board from "components/base/Board";
 import canCompleteByCrafting from "util/fns/depot/canCompleteByCrafting";
 import { LocalStorageSettings } from "types/localStorageSettings";
@@ -42,6 +43,8 @@ const MaterialsNeeded = React.memo((props: Props) => {
   const [popoverItemId, setPopoverItemId] = useState<string | null>(null);
 
   const [exportImportOpen, setExportImportOpen] = useState<boolean>(false);
+
+  const [summaryOpen, setSummaryOpen] = useState<boolean>(false);
 
   const {setAnchorEl, menuProps, menuButtonProps} = useMenu();
 
@@ -78,6 +81,7 @@ const MaterialsNeeded = React.memo((props: Props) => {
   const [ rawValues, setRawValues ] = useState({} as Record<string, DepotItem>);
   //states to keep data beetwen renders
   const [ savedStates, setSavedStates ] = useState({
+    goalsMaterials: {} as Record<string, number>,
     materialsNeeded: {} as Record<string, number>,
     craftableItems: {} as Record<string, boolean>,
     sortedMaterialsNeeded: [] as [string, number][],
@@ -99,6 +103,8 @@ const MaterialsNeeded = React.memo((props: Props) => {
           materialsNeeded[exp] = 0;
         });
       }
+
+      const goalsMaterials = {...materialsNeeded}; 
 
       // 3. calculate what ingredients can be fulfilled by crafting
       const _depot = { ...depot, ..._rawValues }; // need to hypothetically deduct from stock
@@ -132,6 +138,7 @@ const MaterialsNeeded = React.memo((props: Props) => {
       });
 
       setSavedStates({
+        goalsMaterials,
         materialsNeeded,
         craftableItems,
         sortedMaterialsNeeded,
@@ -359,22 +366,27 @@ const MaterialsNeeded = React.memo((props: Props) => {
     <>
       <Board
         title={
-          !depotIsUnsaved ?
-          <Box display="flex" alignItems="center">
-            <Typography variant="h2">Depot</Typography>
-            </Box>
-            : (<Box display="flex"
-              alignItems="center"
-              sx={{ flexFlow: "row nowrap", gap: { xs: 1, md: 3 } }}>
+          !depotIsUnsaved
+            ? <Box display="flex" alignItems="center" paddingRight={1}>
               <Typography variant="h2">Depot</Typography>
+            </Box>
+            : (<Box display="flex" alignItems="center" paddingRight={1}
+              sx={{ flexFlow: "row nowrap", gap: { xs: 1, md: 3 } }}>
+              <Typography variant="h2" sx={{ color: { xs: "primary.main", lg: "unset" } }}>Depot</Typography>
               <Tooltip title="on 5s idle: ↑ upload changes to DB ↑">
                 <Typography variant="h3"
-                  sx={{ color: "#FFD440" }}>unsaved</Typography>
+                  sx={{ color: "primary.main", display: { xs: "none", lg: "unset" } }}>unsaved</Typography>
               </Tooltip>
             </Box>)
         }
         TitleAction={
           <Box display="flex" gap={1}>
+            <Button
+              onClick={() => setSummaryOpen(true)}
+              variant="contained"
+              color="primary">
+              Summary
+            </Button>
             <Tooltip arrow title={craftToggleTooltips[craftToggle]}>
               <Button
                 onClick={handleCraftingToggleAll}
@@ -442,6 +454,7 @@ const MaterialsNeeded = React.memo((props: Props) => {
               xs: "repeat(auto-fill, minmax(96px, 1fr))",
               sm: "repeat(auto-fill, minmax(108px, 1fr))",
             },
+            position: "relative",
           }}
         >
           {savedStates.sortedMaterialsNeeded.map(([itemId, needed]) => (
@@ -480,6 +493,16 @@ const MaterialsNeeded = React.memo((props: Props) => {
           setExportImportOpen(false);
         }}
         goals={goalData}
+      />
+      <MaterialsSummaryDialog
+        depot={depot}
+        goalData={goalData}
+        expOwned={savedStates.expOwned}
+        goalsMaterials={savedStates.goalsMaterials}
+        open={summaryOpen}
+        onClose={() => {
+          setSummaryOpen(false);
+        }}
       />
     </>
   );
