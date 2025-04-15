de9c09233df4bf124a4e805aae3618decabe4915
neia <51728998+neeia@users.noreply.github.com>
2025-04-14 21:08:51 -0700

    Prettier

--- src/util/hooks/useDepot.ts
+++ src/util/hooks/useDepot.ts
@@ -10,40 +10,36 @@ type Depot = Record<string, DepotItem>;
 
 function useDepot() {
   const [depot, _setDepot] = useLocalStorage<Depot>("v3_depot", {});
-  const [depotTrackers, setDepotTrackers ] = useState({
+  const [depotTrackers, setDepotTrackers] = useState({
     rawUpdate: {} as Depot,
     ogValues: {} as Depot,
   });
   const debounceSyncDelay = 5000; //5s of no changes before updating db
 
   //Function for direct upsert to db and reset change trackers
-  const syncDepotToDB = useCallback(
-    async (items: DepotItem[]) => {
-      if (items.length === 0) return;
-      //Docs && Yesod30: "Add filters to every query", so user_id:
-      const { data: { session } } = await supabase.auth.getSession();
-      const user_id = session?.user.id;
-      if (!user_id) return;
+  const syncDepotToDB = useCallback(async (items: DepotItem[]) => {
+    if (items.length === 0) return;
+    //Docs && Yesod30: "Add filters to every query", so user_id:
+    const {
+      data: { session },
+    } = await supabase.auth.getSession();
+    const user_id = session?.user.id;
+    if (!user_id) return;
 
-      const { error } = await supabase.from("depot").upsert(items).eq('user_id', user_id);
-      if (error) {
-        handlePostgrestError(error);
-      }
-      else {
-        setDepotTrackers({rawUpdate: {},ogValues: {}});
-      }
+    const { error } = await supabase.from("depot").upsert(items).eq("user_id", user_id);
+    if (error) {
+      handlePostgrestError(error);
+    } else {
+      setDepotTrackers({ rawUpdate: {}, ogValues: {} });
     }
-    , []
-  )
+  }, []);
 
   //Debounced call of syncDepotToDB
   const debouncedSyncDepot = useCallback(
-    debounce(
-      (items: DepotItem[]) => {
-        syncDepotToDB(items);
-      }
-      , debounceSyncDelay)
-    , [syncDepotToDB]
+    debounce((items: DepotItem[]) => {
+      syncDepotToDB(items);
+    }, debounceSyncDelay),
+    [syncDepotToDB]
   );
 
   // update local storage and agregate depot changes into rawDepotUpdate
@@ -54,16 +50,16 @@ function useDepot() {
       //mixing items with/w/o user_id, provokes row-level security error in upsert
       const depotCopy: Depot = {};
       for (const key in depot) {
-        const { user_id, ...rest } = depot[key]; 
+        const { user_id, ...rest } = depot[key];
         depotCopy[key] = rest;
       }
       const _rawDepotUpdate = { ...depotTrackers.rawUpdate };
-      const _ogDepotValues = { ...depotTrackers.ogValues};
+      const _ogDepotValues = { ...depotTrackers.ogValues };
       let hasChanges = false;
 
       items.forEach((item) => {
         //need to create item in local storage, if it didnt exist
-        //if exist - agregate only changes     
+        //if exist - agregate only changes
         if ((depotCopy[item.material_id]?.stock ?? 0) !== item.stock) {
           //keep og item before first change.
           if (!_ogDepotValues[item.material_id] && depotCopy[item.material_id])
@@ -71,8 +67,8 @@ function useDepot() {
           //ignore user_id
           depotCopy[item.material_id] = {
             material_id: item.material_id,
-            stock: item.stock
-          }
+            stock: item.stock,
+          };
           //ensure stock change from og value
           if (depotCopy[item.material_id].stock !== (_ogDepotValues[item.material_id]?.stock ?? 0)) {
             _rawDepotUpdate[item.material_id] = { ...depotCopy[item.material_id] };
@@ -81,19 +77,17 @@ function useDepot() {
             delete _rawDepotUpdate[item.material_id];
           }
           hasChanges = true;
-        } 
+        }
       });
 
       if (!hasChanges) return; //less function calls
 
       _setDepot(depotCopy);
-      setDepotTrackers({rawUpdate: _rawDepotUpdate, ogValues: _ogDepotValues});
+      setDepotTrackers({ rawUpdate: _rawDepotUpdate, ogValues: _ogDepotValues });
 
-      (immediate) ?
-      syncDepotToDB(Object.values(_rawDepotUpdate))
-      : debouncedSyncDepot(Object.values(_rawDepotUpdate));
+      immediate ? syncDepotToDB(Object.values(_rawDepotUpdate)) : debouncedSyncDepot(Object.values(_rawDepotUpdate));
     },
-    [depot,_setDepot,debouncedSyncDepot,depotTrackers.ogValues,depotTrackers.rawUpdate,syncDepotToDB]
+    [depot, _setDepot, debouncedSyncDepot, depotTrackers.ogValues, depotTrackers.rawUpdate, syncDepotToDB]
   );
 
   //export function to set zero values to local storage and db
@@ -105,8 +99,7 @@ function useDepot() {
     }
     _setDepot(depotCopy);
     syncDepotToDB(Object.values(depotCopy));
-  }, [depot, _setDepot, syncDepotToDB]
-  );
+  }, [depot, _setDepot, syncDepotToDB]);
 
   // fetch data from db
   useEffect(() => {
@@ -130,7 +123,8 @@ function useDepot() {
             //depotResult[x.material_id] = x;
             depotResult[x.material_id] = {
               material_id: x.material_id,
-              stock: x.stock};
+              stock: x.stock,
+            };
           } else {
             depotTrash.push(x.material_id);
           }
@@ -154,11 +148,8 @@ function useDepot() {
   //export function to refresh debounce timer with current changes data
   const refreshDebounce = useCallback(() => {
     //not do anything without active debounce
-    if (hasUnsavedChanges)
-      debouncedSyncDepot(Object.values(depotTrackers.rawUpdate));
-  }
-    , [debouncedSyncDepot, depotTrackers.rawUpdate, hasUnsavedChanges]
-  )
+    if (hasUnsavedChanges) debouncedSyncDepot(Object.values(depotTrackers.rawUpdate));
+  }, [debouncedSyncDepot, depotTrackers.rawUpdate, hasUnsavedChanges]);
 
   return [depot, putDepot, resetDepot, hasUnsavedChanges, refreshDebounce] as const;
 }
