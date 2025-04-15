3a5c5842042d11d3482753b99818a757b492c9f5
neia <51728998+neeia@users.noreply.github.com>
2025-04-15 01:59:39 -0700

    Fix image urls

--- src/util/fns/getAvatarFull.ts
+++ src/util/fns/getAvatarFull.ts
@@ -2,12 +2,14 @@ import { OpInfo } from "types/operators/operator";
 import imageBase from "util/imageBase";
 
 export default function getAvatarFull(op: OpInfo) {
-  if (op.skin) return `${imageBase}/characters/${op.skin.replace("#", "%23")}.webp`;
+  if (op.skin) return `${imageBase}/characters/${op.skin.replace("#", "%23").replace("+", "%2b")}.webp`;
 
   let intermediate = op.op_id;
   if (op?.elite === 2) {
     intermediate += "_2";
   } else if (op?.elite === 1 && op.name === "Amiya") {
+    intermediate += "_1%2b";
+  } else {
     intermediate += "_1";
   }
   return `${imageBase}/characters/${intermediate}.webp`;
