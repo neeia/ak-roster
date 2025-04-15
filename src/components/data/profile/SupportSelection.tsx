a5d64aa4decabdc784fd3beb56dde02125b53ba4
neia <51728998+neeia@users.noreply.github.com>
2025-04-14 21:06:07 -0700

    Remove skill and module from support

--- src/components/data/profile/SupportSelection.tsx
+++ src/components/data/profile/SupportSelection.tsx
@@ -1,13 +1,11 @@
 import { Box, Button, Dialog, DialogActions, DialogContent, DialogTitle, useMediaQuery, useTheme } from "@mui/material";
-import React, { Fragment, useState } from "react";
+import React, { useState } from "react";
 import { OperatorData } from "types/operators/operator";
 import operatorJson from "data/operators";
 import OpSelectionButton from "./OpSelectionButton";
-import Image from "next/image";
 import { OperatorSupport } from "types/operators/supports";
 import useOperators from "util/hooks/useOperators";
 import OperatorSearch from "components/planner/OperatorSearch";
-import SupportOption from "./SupportOption";
 
 interface Props {
   supports: OperatorSupport[];
@@ -27,25 +25,9 @@ const SupportSelection = (props: Props) => {
 
   const setSupport = (value: string) => {
     const support: OperatorSupport = {
-      module: null,
       op_id: value,
-      skill: 0,
       slot: index,
     };
-    setSupports(support);
-  };
-  const setSkill = (supportSlot: number, skillSlot: number) => {
-    const support = supports.find(({ slot }) => slot === supportSlot);
-    if (!support) return;
-    support.skill = skillSlot;
-    setSupports(support);
-  };
-  const setModule = (supportSlot: number, moduleSlot: number) => {
-    const support = supports.find(({ slot }) => slot === supportSlot);
-    if (!support) return;
-    const moduleData = operatorJson[support.op_id].moduleData;
-    if (!moduleData) return;
-    support.module = moduleData[moduleSlot].moduleId;
     setSupports(support);
   };
 
@@ -53,137 +35,35 @@ const SupportSelection = (props: Props) => {
     operators![op.id] != null && !supports!.find((v) => !v || v.op_id === op.id) && (index ? true : op.rarity < 6);
 
   return !operators || !supports ? null : (
-    <>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "8px",
+      }}
+    >
+      <Box>Support Units</Box>
       <Box
         sx={{
-          display: "grid",
-          gridTemplateColumns: "auto 1fr 1fr",
-          gridTemplateRows: "auto 1fr 1fr 1fr",
-          gap: "8px 24px",
-          mr: "8px",
+          display: "flex",
+          gap: "16px",
           alignItems: "center",
         }}
       >
-        <Box>Support Units</Box>
-        <Box>Skills</Box>
-        <Box>Modules</Box>
         {[...Array(3)].map((_, i) => {
           const support = supports.filter((support) => support.slot === i)[0];
           const opA = operators[support?.op_id];
           const opB = operatorJson[support?.op_id];
           const op = support ? { ...opA, ...opB } : undefined;
           return (
-            <Fragment key={i}>
-              <OpSelectionButton
-                op={op}
-                onClick={() => {
-                  setIndex(i);
-                  setOpen(true);
-                }}
-              />
-              {opA && opB && op ? (
-                <>
-                  {/* Skills */}
-                  {!op.skillData ? (
-                    <Box></Box>
-                  ) : (
-                    <SupportOption value={support.skill} onChange={(j) => setSkill(i, j)} mobile={fullScreen}>
-                      {op.skillData.map((_, j) => (
-                        <Box
-                          key={j}
-                          sx={{
-                            position: "relative",
-                            width: 48,
-                            height: 48,
-                          }}
-                        >
-                          <Image
-                            src={`/img/skills/${op.skillData?.[j].iconId ?? op.skillData?.[j].skillId}.png`}
-                            width={48}
-                            height={48}
-                            alt={`Skill ${j + 1}`}
-                          />
-                          <Box
-                            sx={{
-                              position: "absolute",
-                              right: -4,
-                              bottom: -4,
-                              width: 24,
-                              height: 24,
-                            }}
-                          >
-                            <Image src={`/img/rank/bg.png`} fill alt={""} />
-                            {!op.masteries[j] || op.masteries[j] === 0 ? (
-                              <Image src={`/img/rank/${op.skill_level}.png`} fill alt={`Level ${op.skill_level}`} />
-                            ) : (
-                              <Image
-                                src={`/img/rank/m-${op.masteries[j]}.png`}
-                                fill
-                                alt={`Mastery Level ${op.masteries[j]}`}
-                              />
-                            )}
-                          </Box>
-                        </Box>
-                      ))}
-                    </SupportOption>
-                  )}
-                  {/* Modules */}
-                  {!op.moduleData ? (
-                    <Box></Box>
-                  ) : (
-                    <SupportOption
-                      value={op.moduleData.findIndex(({ moduleId }) => moduleId === support.module) ?? null}
-                      onChange={(j) => setModule(i, j)}
-                      mobile={fullScreen}
-                      disable={op.moduleData.reduce(
-                        (acc, cur, i) => (op.modules[cur.moduleId] > 0 ? acc : [...acc, i]),
-                        [] as number[]
-                      )}
-                    >
-                      {op.moduleData.map((mod, j) => {
-                        const stage = op.modules[mod.moduleId];
-                        return (
-                          <Box
-                            key={j}
-                            sx={{
-                              position: "relative",
-                              width: 48,
-                              height: 48,
-                            }}
-                          >
-                            <Image
-                              src={`/img/equip/${mod.moduleId}.png`}
-                              width={48}
-                              height={48}
-                              alt={`Module ${j + 1}`}
-                            />
-                            {stage > 0 && (
-                              <Box
-                                sx={{
-                                  position: "absolute",
-                                  right: -4,
-                                  bottom: -4,
-                                  width: 24,
-                                  height: 24,
-                                }}
-                              >
-                                <Image src={`/img/rank/bg.png`} fill alt={""} />
-                                <Image src={`/img/equip/img_stg${stage}.png`} fill alt={`Stage ${stage}`} />
-                              </Box>
-                            )}
-                          </Box>
-                        );
-                      })}
-                    </SupportOption>
-                  )}
-                </>
-              ) : (
-                <>
-                  <div />
-                  <div />
-                </>
-              )}
-            </Fragment>
+            <OpSelectionButton
+              key={i}
+              op={op}
+              onClick={() => {
+                setIndex(i);
+                setOpen(true);
+              }}
+            />
           );
         })}
       </Box>
@@ -230,7 +110,7 @@ const SupportSelection = (props: Props) => {
           </Button>
         </DialogActions>
       </Dialog>
-    </>
+    </Box>
   );
 };
 
--- src/types/operators/supports.ts
+++ src/types/operators/supports.ts
@@ -1,6 +1,4 @@
 export interface OperatorSupport {
-  module: string | null;
   op_id: string;
-  skill: number;
   slot: number;
 }
