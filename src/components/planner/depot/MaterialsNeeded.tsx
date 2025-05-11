919669c46d6d432b4cc0d02d91c669b31aa86371
neia <neeia2718@gmail.com>
2025-05-11 15:15:46 -0700

    Revert "Show tracked events as item sources in popover."
    This reverts commit cc43da6ee8cebeebb209e4184945ce62f6a5b749.

--- src/components/planner/depot/MaterialsNeeded.tsx
+++ src/components/planner/depot/MaterialsNeeded.tsx
@@ -533,8 +533,6 @@ const MaterialsNeeded = React.memo((props: Props) => {
           ingredientToCraftedItemsMapping={savedStates.ingredientToCraftedItemsMapping}
           open={popoverOpen}
           onClose={handlePopoverClose}
-          openEventTracker={() => setEventsTrackerOpen(true)}
-          eventsData={Object.keys(eventsData ?? {}).length > 0 ? eventsData : trackerDefaults?.eventsData ?? {}}
         />
       </Board>
       <ExportImportDialog
