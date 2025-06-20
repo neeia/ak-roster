aa99980e941ce971ce68ab017047cc4837725c02
neia <51728998+neeia@users.noreply.github.com>
2025-06-19 18:38:45 -0700

    Add route to fetch user data

---
+++ src/pages/api/u/[user].tsx
@@ -0,0 +1,56 @@
+import { createClient } from "@supabase/supabase-js";
+import operatorJson from "data/operators";
+import { NextApiRequest, NextApiResponse } from "next";
+import { Operator } from "types/operators/operator";
+import Roster from "types/operators/roster";
+import { Database } from "types/supabase";
+
+export default async function (req: NextApiRequest, res: NextApiResponse) {
+  const user = (req.url?.split("/u/")[1] ?? "")[1];
+  if (!user) {
+    res.status(400).send("No username provided.");
+    return;
+  }
+
+  const username = user.toString().replace(",", "");
+
+  const supabase = createClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
+
+  const { data: account, error: accountError } = await supabase
+    .from("krooster_accounts")
+    .select("*, supports (op_id, slot), operators (*)")
+    .eq("username", username.toLocaleLowerCase())
+    .limit(1)
+    .single();
+
+  if (!account) {
+    res.status(404).send("User not found");
+    return;
+  }
+
+  if (accountError) {
+    res.status(500).send("Internal server error.");
+    return;
+  }
+
+  const { supports, operators, ...accountData } = account;
+  const roster = (operators as Operator[]).reduce((roster: Roster, op: Operator) => {
+    if (op.op_id in operatorJson) {
+      roster[op.op_id] = { ...op } as Operator;
+    }
+    return roster;
+  }, {});
+
+  console.log(accountData);
+
+  res.status(200).json({
+    data: {
+      account: accountData,
+      supports,
+      roster,
+    },
+  });
+}
